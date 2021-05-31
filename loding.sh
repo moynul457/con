513f5e3822dc410325a91a46da66a5cb4616e4aa
moynul457 <69568834+moynul457@users.noreply.github.com>
2021-05-31 08:33:34 +0600

    Add files via upload

---
+++ loding.sh
@@ -0,0 +1,404 @@
+command_not_found_handle() {
+	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+
+clear
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo"" 
+echo""
+echo""
+echo""
+echo""
+echo  ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo""
+echo""
+echo""
+echo""
+echo "               "/█1%...................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█4%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█8%......................./
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██10%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██12%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███15%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "               "/█████19%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "               "/██████28%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/████████30%................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██████████40%................/
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█████████████50%............./
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/████████████████60%........../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███████████████████88%......./
+sleep 2.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo ""
+echo "" 
+echo "               "[██████████████████████ 100%]
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 1.0             "
+ /$$$$$$$$                                                          /$$                 /$$                                                 /$$                               /$$             /$$      
+|__  $$__/                                                         | $$                | $$                                                | $$                              | $$            | $$      
+   | $$  /$$$$$$   /$$$$$$  /$$$$$$/$$$$  /$$   /$$ /$$   /$$      | $$$$$$$   /$$$$$$ | $$  /$$$$$$         /$$$$$$$  /$$$$$$  /$$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$       | $$$$$$$   /$$$$$$$      
+   | $$ /$$__  $$ /$$__  $$| $$_  $$_  $$| $$  | $$|  $$ /$$/      | $$__  $$ /$$__  $$| $$ /$$__  $$       /$$_____/ /$$__  $$| $$__  $$|_  $$_/   /$$__  $$ /$$__  $$      | $$__  $$ /$$__  $$      
+   | $$| $$$$$$$$| $$  \__/| $$ \ $$ \ $$| $$  | $$ \  $$$$/       | $$  \ $$| $$$$$$$$| $$| $$  \ $$      | $$      | $$$$$$$$| $$  \ $$  | $$    | $$$$$$$$| $$  \__/      | $$  \ $$| $$  | $$      
+   | $$| $$_____/| $$      | $$ | $$ | $$| $$  | $$  >$$  $$       | $$  | $$| $$_____/| $$| $$  | $$      | $$      | $$_____/| $$  | $$  | $$ /$$| $$_____/| $$            | $$  | $$| $$  | $$      
+   | $$|  $$$$$$$| $$      | $$ | $$ | $$|  $$$$$$/ /$$/\  $$      | $$  | $$|  $$$$$$$| $$| $$$$$$$/      |  $$$$$$$|  $$$$$$$| $$  | $$  |  $$$$/|  $$$$$$$| $$            | $$$$$$$/|  $$$$$$$      
+   |__/ \_______/|__/      |__/ |__/ |__/ \______/ |__/  \__/      |__/  |__/ \_______/|__/| $$____/        \_______/ \_______/|__/  |__/   \___/   \_______/|__/            |_______/  \_______/      
+                                                                                           | $$                                                                                                        
+                                                                                           | $$                                                                                                        
+                                                                                           |__/                                                                                                        
+                          /$$$$$$   /$$$$$$  /$$   /$$       /$$$$$$$                                                                                                                                  
+                         /$$__  $$ /$$$_  $$| $$  /$$/      | $$__  $$                                                                                                                                 
+                        |__/  \ $$| $$$$\ $$| $$ /$$/       | $$  \ $$  /$$$$$$  /$$$$$$$   /$$$$$$                                                                                                    
+                           /$$$$$/| $$ $$ $$| $$$$$/        | $$  | $$ /$$__  $$| $$__  $$ /$$__  $$                                                                                                   
+                          |___  $$| $$\ $$$$| $$  $$        | $$  | $$| $$  \ $$| $$  \ $$| $$$$$$$$                                                                                                   
+                         /$$  \ $$| $$ \ $$$| $$\  $$       | $$  | $$| $$  | $$| $$  | $$| $$_____/                                                                                                   
+                        |  $$$$$$/|  $$$$$$/| $$ \  $$      | $$$$$$$/|  $$$$$$/| $$  | $$|  $$$$$$$                                                                                                   
+                         \______/  \______/ |__/  \__/      |_______/  \______/ |__/  |__/ \_______/                                                                                                   
+                                                                                                                                                                                                       
+                                                                                                                                                                                                       
+                                                                                                                                                                                                       
+
+clear
